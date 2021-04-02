8b9fbb94ed65f82a699205effc258127217dd8cb
Siraj Pathan <shirazpathanh043617@gmail.com>
2021-04-02 23:36:40 +0530

    added attributes to user.java

--- onlineExam2/src/main/java/com/lti/entity/User.java
+++ onlineExam2/src/main/java/com/lti/entity/User.java
@@ -1,5 +1,78 @@
 package com.lti.entity;
 
-public class User {
+import java.time.LocalDate;
 
+public class User {
+    private int id;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String email;
+    private LocalDate dateOfBirth;
+    private String gender;
+    private int phoneNo;
+    private String password;
+    private String qualification;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getMiddleName() {
+        return middleName;
+    }
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public int getPhoneNo() {
+        return phoneNo;
+    }
+    public void setPhoneNo(int phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getQualification() {
+        return qualification;
+    }
+    public void setQualification(String qualification) {
+        this.qualification = qualification;
+    }
+    
 }
+ 
