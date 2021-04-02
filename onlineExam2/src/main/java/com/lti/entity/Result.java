edbe1489e24a94359b0ead2421a3a9bda4e04ef5
Siraj Pathan <shirazpathanh043617@gmail.com>
2021-04-03 00:05:55 +0530

    added result.java added userAnswer.java

--- onlineExam2/src/main/java/com/lti/entity/Result.java
+++ onlineExam2/src/main/java/com/lti/entity/Result.java
@@ -1,5 +1,28 @@
 package com.lti.entity;
 
 public class Result {
+	private int id;
+	private float score;
+	private int attempts;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public float getScore() {
+		return score;
+	}
+	public void setScore(float score) {
+		this.score = score;
+	}
+	public int getAttempts() {
+		return attempts;
+	}
+	public void setAttempts(int attempts) {
+		this.attempts = attempts;
+	}
+	
+	
 
 }
--- onlineExam2/src/main/java/com/lti/entity/UserAnswer.java
+++ onlineExam2/src/main/java/com/lti/entity/UserAnswer.java
@@ -1,5 +1,22 @@
 package com.lti.entity;
 
 public class UserAnswer {
+	private int id;
+	private String answerGiven;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getAnswerGiven() {
+		return answerGiven;
+	}
+	public void setAnswerGiven(String answerGiven) {
+		this.answerGiven = answerGiven;
+	}
+
+	
 
 }
